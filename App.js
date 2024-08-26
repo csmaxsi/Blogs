156960bdaf9094f671ce6a669b4aafcef9cb9c77
csmaxsi <plchathurasankalpa@gmail.com>
2024-08-26 16:27:46 +0530

    Initial Commit

--- App.js
+++ App.js
@@ -1,12 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StyleSheet, Text, View } from 'react-native';
+import IndexScreen from './src/screens/IndexScreen';
+
+const Stack = createNativeStackNavigator();
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+   <NavigationContainer>
+      <Stack.Navigator initialRouteName="Blogs">
+        <Stack.Screen name="Blogs" component ={IndexScreen} />
+      </Stack.Navigator>
+   </NavigationContainer>
+   
   );
 }
 
